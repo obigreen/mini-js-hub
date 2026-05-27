2ed87cacf826c3cb9da2fffb352019c636748710
Sergey Budyakin <youshtuchka@gmail.com>
2026-05-27 20:34:11 +0300

    worked in timer script.js

--- components/to-do-list_1/script.js
+++ components/to-do-list_1/script.js
@@ -10,6 +10,49 @@ $(function () {
         return;
     }
 
-    console.log(todoInput.length, todoAdd.length, todoList.length);
 
+    todoAdd.on("click", function () {
+
+        const todoText = todoInput.val().trim();
+
+        if (todoText === "") {
+            console.log("Введите текст в поле")
+            return;
+        }
+
+
+        const newLine = $(`
+              <li class="todo-item">
+                        <span class="todo-text">${todoText}</span>
+                        <button class="todo-check" type="button" aria-label="Mark task as done">✔</button>
+                        <button class="todo-delete" type="button" aria-label="Delete task">✖</button>
+                    </li>
+                `)
+
+        newLine.hide(300);
+        todoList.prepend(newLine);
+        newLine.fadeIn("fast");
+
+
+        todoInput.val("");
+    })
+
+    todoInput.on("keyup", function (event) {
+
+        if (event.key === 'Enter') {
+            todoAdd.click();
+        }
+
+    })
+
+
+    todoList.on("click", ".todo-check", function () {
+        $(this).closest(".todo-item").find(".todo-text").toggleClass("done");
+    })
+
+    todoList.on("click", ".todo-delete", function () {
+        $(this).closest(".todo-item").fadeOut("fast", function () {
+            $(this).remove();
+        });
+    })
 })
