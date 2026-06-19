1a0548961b86f9f65d0a1aa74d280df09ebae751
Sergey Budyakin <youshtuchka@gmail.com>
2026-06-19 20:42:04 +0300

    progress step component is done

--- components/step_indicator/script.js
+++ components/step_indicator/script.js
@@ -2,8 +2,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const stepsContainer = document.querySelector('.steps');
     const stepItems = document.querySelectorAll('.step');
+    const prevBtn = document.querySelector('[data-action="prev"]');
+    const nextBtn = document.querySelector('[data-action="next"]');
 
-    if (!stepsContainer || !stepItems.length) {
+    if ((!stepsContainer || !stepItems.length) || (!prevBtn || !nextBtn)) {
         return;
     }
 
@@ -11,47 +13,46 @@ document.addEventListener('DOMContentLoaded', () => {
     let currentStep = 0;
     const currentClass = 'step_current';
     const doneClass = 'step_done';
+    const maxStep = stepItems.length - 1;
 
 
     function renderStep(targetStep) {
         stepItems.forEach((step, index) => {
-            step.classList.remove(currentClass || doneClass)
+            step.classList.remove(currentClass, doneClass)
 
-
+            if (targetStep === index) {
+                step.classList.add(currentClass);
+            } else if (targetStep > index) {
+                step.classList.add(doneClass);
+            }
         })
+
+        updateButtons();
 
     }
 
-})
+    function updateButtons() {
+        prevBtn.disabled = currentStep <= 0;
+        nextBtn.disabled = currentStep >= maxStep;
 
+    }
 
-/*
-
-        if (...) {
-            return;
+    prevBtn.addEventListener('click', () => {
+        if (currentStep > 0) {
+            currentStep--;
+            renderStep(currentStep);
         }
-
-        let currentStep = 0;
-        const currentClass = "...";
-        const doneClass = "...";
-
-        function renderStep(targetStep) {
-            stepItems.forEach((step, index) => {
-                step.classList.remove(...);
-
-                if (...) {
-                    step.classList.add(...);
-                } else if (...) {
-                    step.classList.add(...);
-                }
-            });
-        }
-
-        renderStep(currentStep);
     });
 
-    Проверка:
-    после написания кода первый шаг должен стать current автоматически.
-    Если временно поменять currentStep на 2, первые два шага должны стать done,
-    третий current, а линии после done-шагов должны заполниться.
-*/
+
+    nextBtn.addEventListener('click', () => {
+        if (currentStep < maxStep) {
+            currentStep++;
+            renderStep(currentStep);
+        }
+    });
+
+
+    renderStep(currentStep);
+
+})
